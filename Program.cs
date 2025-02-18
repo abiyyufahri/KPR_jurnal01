3a6a28cec86fa04f4a6b9a26b0c444031ccf88a0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-02-19 06:59:20 +0700

    feat : password

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Enter username:");
 // Membuat variabel username untuk menerima input user
 string userName = Console.ReadLine();
+Console.WriteLine("Enter password:");
+// Membuat variabel username untuk menerima input user
+string pass = Console.ReadLine();
 // Melakukan print panjang string input
 Console.WriteLine("The length of the input string is: " + userName.Length);
 // Melakukan print dengan menggabungkan string dengan input user
 Console.WriteLine("Username is: " + userName);
+Console.WriteLine("Password is: " + pass);
