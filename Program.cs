116d93f2f67b685c3f6edd818fc685842359c259
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-02-19 06:53:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter username:");
+// Membuat variabel username untuk menerima input user
+string userName = Console.ReadLine();
+// Melakukan print panjang string input
+Console.WriteLine("The length of the input string is: " + userName.Length);
+// Melakukan print dengan menggabungkan string dengan input user
+Console.WriteLine("Username is: " + userName);
